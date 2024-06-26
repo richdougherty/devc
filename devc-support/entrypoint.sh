a3864a7b034cdc3812bc5ba9d13a5b7b3ad8f72d
Rich Dougherty <rich@rd.nz>
2024-06-27 07:55:46 +1200

    Fix user/group creation in support container

--- devc-support/entrypoint.sh
+++ devc-support/entrypoint.sh
@@ -21,30 +21,39 @@ fi
 
 verbose_log "Starting with UID : $LOCAL_USER_ID, GID : $LOCAL_GROUP_ID"
 
-# Create or modify group
+# Handle group
 if getent group $LOCAL_GROUP_ID > /dev/null 2>&1; then
-    verbose_log "Modifying existing group with GID $LOCAL_GROUP_ID"
-    groupmod -g $LOCAL_GROUP_ID $(getent group $LOCAL_GROUP_ID | cut -d: -f1)
+    GROUP_NAME=$(getent group $LOCAL_GROUP_ID | cut -d: -f1)
+    verbose_log "Group with GID $LOCAL_GROUP_ID already exists: $GROUP_NAME"
 else
-    verbose_log "Creating new group with GID $LOCAL_GROUP_ID"
-    groupadd -g $LOCAL_GROUP_ID usergroup
+    GROUP_NAME="devgroup$LOCAL_GROUP_ID"
+    verbose_log "Creating group $GROUP_NAME with GID $LOCAL_GROUP_ID"
+    groupadd -g $LOCAL_GROUP_ID $GROUP_NAME
 fi
 
-# Create or modify user
-if id user > /dev/null 2>&1; then
-    verbose_log "Modifying existing user with UID $LOCAL_USER_ID"
-    usermod -u $LOCAL_USER_ID -g $LOCAL_GROUP_ID user
+# Handle user
+if id -u $LOCAL_USER_ID > /dev/null 2>&1; then
+    USER_NAME=$(id -nu $LOCAL_USER_ID)
+    OLD_PRIMARY_GID=$(id -g $USER_NAME)
+    verbose_log "User with UID $LOCAL_USER_ID already exists: $USER_NAME"    
+    
+    # Add old primary group as secondary group if it's different
+    if [ "$OLD_PRIMARY_GID" != "$LOCAL_GROUP_ID" ]; then
+        verbose_log "Changing primary group of $LOCAL_USER_ID to $LOCAL_GROUP_ID"
+        usermod -g $LOCAL_GROUP_ID $USER_NAME
+        verbose_log "Adding additional group of old group $OLD_PRIMARY_GID to user $LOCAL_USER_ID"
+        usermod -a -G $OLD_PRIMARY_GID $USER_NAME
+    fi
 else
-    verbose_log "Creating new user with UID $LOCAL_USER_ID"
-    useradd --shell /bin/bash -u $LOCAL_USER_ID -g $LOCAL_GROUP_ID -o -c "" -m user
+    USER_NAME="devuser$LOCAL_USER_ID"
+    verbose_log "Creating user $USER_NAME with UID $LOCAL_USER_ID"
+    useradd -u $LOCAL_USER_ID -g $LOCAL_GROUP_ID -m -s /bin/bash $USER_NAME
 fi
-
-export HOME=/home/user
 
 verbose_log "Ensuring correct ownership of user home and workspace"
 # Ensure the user owns its home directory and the workspace
-chown user:$LOCAL_GROUP_ID $HOME
-chown user:$LOCAL_GROUP_ID /workspace
+chown $USER_NAME:$LOCAL_GROUP_ID $HOME
+chown $USER_NAME:$LOCAL_GROUP_ID /workspace
 
 # Set ACL for Docker socket
 verbose_log "Setting ACL for Docker socket"
@@ -52,4 +61,4 @@ sudo setfacl --modify user:$LOCAL_USER_ID:rw /var/run/docker.sock
 
 verbose_log "Executing command as user: $*"
 # Execute command as user
-exec sudo -E -H -u user bash -c "$*"
+exec sudo -E -H -u $USER_NAME bash -c "$*"
